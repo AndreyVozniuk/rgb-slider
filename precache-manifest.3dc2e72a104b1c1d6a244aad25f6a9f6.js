74ba5850b16aeb7c56d7660e6deb4608ec009e1c
Andrew <vozniukandrey@gmail.com>
2020-11-24 20:16:57 +0200

    Updates

---
+++ precache-manifest.3dc2e72a104b1c1d6a244aad25f6a9f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88afb43f98b8afe1e029fb2a49ab1d59",
+    "url": "/rgb-slider/index.html"
+  },
+  {
+    "revision": "0b09e7da0955eaed6a0a",
+    "url": "/rgb-slider/static/css/main.9475fcc5.chunk.css"
+  },
+  {
+    "revision": "3251a809fe3991b6fafe",
+    "url": "/rgb-slider/static/js/2.2ba27129.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rgb-slider/static/js/2.2ba27129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b09e7da0955eaed6a0a",
+    "url": "/rgb-slider/static/js/main.667f3537.chunk.js"
+  },
+  {
+    "revision": "6c54ddd69e17f215779b",
+    "url": "/rgb-slider/static/js/runtime-main.f9fd310b.js"
+  }
+]);
